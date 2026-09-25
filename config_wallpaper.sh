e93bdd730a8bd51a07986b7d2b0793210b669ff4
pliniopvv <pliniopvv@gmail.com>
2026-09-25 11:35:47 -0300

    +update ~/.fehbg

--- config_wallpaper.sh
+++ config_wallpaper.sh
@@ -1,3 +1,7 @@
-sudo mkdir /usr/share/backgrounds
+sudo mkdir -p /usr/share/backgrounds
 sudo magick -size 1920x1080 xc:black /usr/share/backgrounds/black.png
+
 feh --bg-fill /usr/share/backgrounds/black.png
+cat << EOF > ~/.fehbg
+feh --bg-fill /usr/share/backgrounds/black.png
+EOF
